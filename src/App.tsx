917572ed46ea2152ebcc91ee4c6670691f5745e9
Vlad Muresan <vladmuresan02@gmail.com>
2023-06-17 19:21:15 +0300

    added dist

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect, useRef } from 'react'
+import data from './data/db'
+import logo from '../src/assets/waystar-royco-logo.png'
+import { Transition } from 'react-transition-group';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+
+  const [activeInsult, setActiveInsult] =
+    useState<Record<string, string>>({})
+
+  let [transitionControl, setTransitionControl] = useState(false);
+
+  const nodeRef = useRef(null)
+
+  useEffect(() => {
+    transitionControl = true;
+    setActiveInsult(data[0]);
+  }, [data])
+
+  useEffect(() => {
+    setTransitionControl(false)
+  }, [activeInsult])
+  const transitionDuration = 300
+
+  const newInsult = () => {
+    setTransitionControl(true);
+    setTimeout(() => {
+      const min = Math.ceil(0);
+      const max = Math.floor(Object.keys(data).length);
+      let newIndex = Math.floor(Math.random() * (max - min) + min).toString();
+      setActiveInsult(data[newIndex])
+    }, transitionDuration)
+
+
+
+  }
+  const transitionStyles: Record<string, Record<string, string | number>> = {
+    entering: { opacity: 0.2 },
+    entered: { opacity: 0 },
+    exiting: { opacity: 0.7 },
+    exited: { opacity: 1 },
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <main className="app-bg bg-no-repeat flex items-center justify-center flex-col sm:pb-24 sm:px-32 pb-16 px-8" >
+        <img src={logo} alt="" className='mb-8 mt-3 ' width={150} />
+        {activeInsult &&
+          <Transition nodeRef={nodeRef} in={transitionControl} timeout={transitionDuration
+          }>
+            {state => (
+              <section className="  mt-auto mb-auto  text-gray-100 w-full sm:w-4/6 transition-all  ease-in-out " style={{ ...transitionStyles[state] }}>
+                <h3 className='tracking-widest sm:text-3xl/10  text-xl/10 '>{activeInsult.insult}</h3>
+                <div className="insult-after flex items-center flex-row  mt-6 w-full">
+                  <div className="line  bg-white grow mr-2  opacity-20 h-0.5"></div>
+                  <p className='text-uppercase tracking-big uppercase text-sm opacity-50 sm:text-md text-gray-300 font-thin'>{activeInsult.character}</p>
+                </div>
+              </section>
+            )}
+          </Transition>
+        }
+        <button type="button" className='
+         px-8 py-3 color-white  text-gray-100 bg-gray-950  tracking-widest hover:bg-gray-100 hover:text-gray-950 hover:tracking-big transition-all ease-in-out duration-500  mb-0' onClick={newInsult}>
+          NEW INSULT
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+        <div>
+
+        </div>
+      </main>
     </>
   )
 }
